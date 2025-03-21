bae72c98e69732e99d4d8738b3b7bcc97f534d32
Suganya Ponnusamy <suganya.sugu88@gmail.com>
2025-03-20 19:44:12 -0500

    Sucessful run

--- practice-character-checker.js
+++ practice-character-checker.js
@@ -2,6 +2,6 @@ const readlineSync = require('readline-sync');
 // Prompt user for a string input
 const userInput = readlineSync.question('Enter a string: ');
 // Prompt user for an index number
-const index = readlineSync.question('Enter an index number: ');
+const stringIndex = readlineSync.question('Enter an index number: ');
 // Print out the character at the given index
-console.log(`Character at index ${index}: ${userInput[index]}`);
+console.log(`Character at index ${stringIndex}: ${userInput[stringIndex]}`);
